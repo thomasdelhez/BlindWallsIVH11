42de40a1389b7cb1a4e6513c2aa833497fc37c03
Thomas Delhez <thomasdelhez@live.nl>
2018-02-07 10:37:30 +0100

    delete

--- src/main/java/nl/avans/ivh11/DemoApplication/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package nl.avans.ivh11.DemoApplication;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
