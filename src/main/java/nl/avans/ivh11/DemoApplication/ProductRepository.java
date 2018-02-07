763150d6b2541d76aa0130dfa1ed7d60dfd31193
Thomas Delhez <thomasdelhez@live.nl>
2018-02-07 10:19:05 +0100

    first commit

---
+++ src/main/java/nl/avans/ivh11/DemoApplication/ProductRepository.java
@@ -0,0 +1,7 @@
+package nl.avans.ivh11.DemoApplication;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+}
