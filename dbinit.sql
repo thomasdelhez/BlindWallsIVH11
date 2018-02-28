7e5509a59658ad19369f56b28bcdffb8b2c726b5
Thomas Delhez <thomasdelhez@live.nl>
2018-02-28 14:26:20 +0100

    database juiste naam

--- dbinit.sql
+++ dbinit.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS `springdemodb`;
-CREATE DATABASE `springdemodb`;
+DROP DATABASE IF EXISTS `blindwallsdb`;
+CREATE DATABASE `blindwallsdb`;
 
 -- aanmaken
 CREATE USER 'demo_user'@'localhost' IDENTIFIED BY 'secret';
 
 -- geef in een keer alle rechten - soort administrator!
-GRANT ALL ON `springdemodb`.* TO 'demo_user'@'localhost';
+GRANT ALL ON `blindwallsdb`.* TO 'demo_user'@'localhost';
